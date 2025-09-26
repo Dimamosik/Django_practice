e55b48986c9b1e6fcef88337d281c283d42c7aee
Hellkite <133752636+Dimamosik@users.noreply.github.com>
2025-09-26 13:46:14 +0200

    BlogNest (#2)
    * BlogNest
    
    * BlogNest1.1
    
    * Delete ck --lost-found

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('',views.index, name='home'),
     path('about',views.about, name='about'),
-    path('contact', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('profile', views.profile, name='profile' )
 ]
--- main/views.py
+++ main/views.py
@@ -1,13 +1,40 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    data ={
+        'title': 'Main page',
+    }
+    return render(request, 'main/index.html', data)
 
 
 def about(request):
     return render(request, 'main/about.html')
 
 def contact(request):
+    return render(request, 'main/contact.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been created! You can now log in.')
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'main/register.html', {'form': form})
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html', {})
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f"{name} ({email}): {message}") 
     return render(request, 'main/contact.html')
