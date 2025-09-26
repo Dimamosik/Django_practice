554c058a393c7986e3270466f5dfc436a5002f05
Hellkite <133752636+Dimamosik@users.noreply.github.com>
2025-09-26 13:41:09 +0200

    Profile update (#4)
    * BlogNest
    
    * BlogNest1.1
    
    * BlogNest1.2
    
    * BlogNest1.5
    
    * Delete ck --lost-found

---
+++ news/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import News
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = ['title', 'content', 'image']
+        labels = {
+            'title': 'Title',
+            'content': 'Content',
+            'image': 'Image',
+        }
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
---
+++ news/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import NewsListView, NewsDetailView, NewsCreateView, NewsUpdateView, NewsDeleteView
+
+urlpatterns = [
+    path('', NewsListView.as_view(), name='news_home'),          
+    path('<int:pk>/', NewsDetailView.as_view(), name='news_detail'),  
+    path('new/', NewsCreateView.as_view(), name='news_create'),       
+    path('<int:pk>/edit/', NewsUpdateView.as_view(), name='news_update'),  
+    path('<int:pk>/delete/', NewsDeleteView.as_view(), name='news_delete'),  
+]
